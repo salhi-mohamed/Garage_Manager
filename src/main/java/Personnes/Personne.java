fd9035adac2e6da4559efe71b24b2dd6ae8fc755
salhi-mohamed <150184561+salhi-mohamed@users.noreply.github.com>
2024-10-12 22:05:55 +0100

    first commit

---
+++ src/main/java/Personnes/Personne.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Personnes;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Personne {
+    private int id;
+    private String nom;
+    private String prenom;
+    private String adresse;
+    private int telephone;
+    public Personne(int id , String nom, String prenom , String adresse , int telephone)
+    {
+        this.id=id;
+        this.nom=nom;
+        this.prenom=prenom;
+        this.adresse=adresse;
+        this.telephone=telephone;
+    }
+    public void set_id(int id)
+    {
+        this.id=id;
+    }
+    public void set_nom(String nom)
+    {
+        this.nom=nom;
+    }
+    public void set_prenom(String prenom)
+    {
+        this.prenom=prenom;
+    }
+    public void set_adresse(String adresse)
+    {
+        this.adresse=adresse;
+    }
+    public void set_telephone(int telephone)
+    {
+        this.telephone=telephone;
+    }
+    public int get_id()
+    {
+        
+    }
+    
+    
+    
+    
+    
+}
